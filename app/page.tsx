0090e7a60d07b3cba15c5a329d07b2587ab1062e
Maxell Milay <maxell@bposeats.com>
2024-03-10 14:09:27 +0800

    [Maxell][UI] changed theme color of site

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import LinePlot from '@/components/home/LinePlot'
+
 export default function Home() {
   return (
-    <main className="flex">
+    <main className="flex px-4">
       <h1>Home Page</h1>
+      <LinePlot />
     </main>
   )
 }
--- components/generics/Header.tsx
+++ components/generics/Header.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import { FaBox, FaCircleUser, FaCaretDown } from 'react-icons/fa6'
+import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div className="flex">
-      <FaBox />
-      <p>ThinkFit</p>
-      <FaCircleUser />
-      <p>Maxell Milay</p>
-      <FaCaretDown />
+    <div className="flex bg-tf-secondary text-white items-center justify-between px-5 py-3">
+      <div className="flex items-center gap-3">
+        <FaBox className="text-xl" />
+        <div className="relative h-10 aspect-[1167/509]">
+          <Image
+            src="/generics/thinkfit-expanded-logo.png"
+            alt="Expanded Logo"
+            fill
+            className="object-cover"
+          ></Image>
+        </div>
+      </div>
+      <div className="flex gap-3 items-center">
+        <FaCircleUser className="text-xl" />
+        <p>Maxell Milay</p>
+        <FaCaretDown />
+      </div>
     </div>
   )
 }
