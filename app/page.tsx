4d150234d8e34e8ac53087c69971d1dfeabfdf67
Maxell Milay <maxell@bposeats.com>
2024-03-10 14:32:57 +0800

    [Maxell][Setup] added favicon

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import LinePlot from '@/components/home/LinePlot';
 
 export default function Home() {
   return (
-    <main className="flex px-4">
+    <main className="flex px-4 py-2">
       <h1>Home Page</h1>
       <LinePlot />
     </main>
--- components/generics/Header.tsx
+++ components/generics/Header.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { FaBox, FaCircleUser, FaCaretDown } from 'react-icons/fa6';
+import { FaCircleUser, FaCaretDown } from 'react-icons/fa6';
 import Image from 'next/image';
 
 const Header = () => {
   return (
     <div className="flex bg-tf-secondary text-white items-center justify-between px-5 py-3">
       <div className="flex items-center gap-3">
-        <FaBox className="text-xl" />
         <div className="relative h-10 aspect-[1167/509]">
           <Image
             src="/generics/thinkfit-expanded-logo.png"
