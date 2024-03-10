2e8094d7f6ba1535e259eafd17bc360059f86827
Maxell Milay <maxell@bposeats.com>
2024-03-10 12:43:40 +0800

    [Maxell][Setup] added prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,23 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "ThinkFit",
-};
+  title: 'ThinkFit',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <head></head>
-        <link rel="stylesheet" href="boxicons.min.css"></link>
+      <link rel="stylesheet" href="boxicons.min.css"></link>
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
   return (
-    <main className='flex'>
+    <main className="flex">
       <h1>Home Page</h1>
     </main>
-  );
+  )
 }
