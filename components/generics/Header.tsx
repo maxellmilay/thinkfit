1c3b8618749c9d4fc663875ce53106cffad25729
Maxell Milay <maxell@bposeats.com>
2024-03-10 12:39:34 +0800

    [Maxell][Setup] setup header component

---
+++ components/generics/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex'>
+      <p>ThinkFit</p>
+      <p>Maxell Milay</p>
+    </div>
+  )
+}
+
+export default Header
