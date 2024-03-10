0169b0d5d7b8f715670979762c4009ccc717129d
Maxell Milay <maxell@bposeats.com>
2024-03-10 12:55:03 +0800

    [Maxell][UI] added content into header component

--- components/generics/Header.tsx
+++ components/generics/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { FaBox, FaCircleUser, FaCaretDown } from 'react-icons/fa6'
 
 const Header = () => {
   return (
     <div className="flex">
+      <FaBox />
       <p>ThinkFit</p>
+      <FaCircleUser />
       <p>Maxell Milay</p>
+      <FaCaretDown />
     </div>
   )
 }
